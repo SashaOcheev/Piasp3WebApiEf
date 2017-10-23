3324f36bb15b36482f1a5b78b594b3149b31fa7f
SashaOcheev <OcheevSasha@gmail.com>
2017-10-23 23:19:16 +0300

    connection string

---
+++ Piasp3WebApiEf/DAL/Common/EntityContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Piasp3WebApiEf.DAL.Common
+{
+    public class EntityContext<TEntity> : DbContext where TEntity : class
+    {
+        public EntityContext()
+            : base("DbConnection")
+        { }
+
+
+        public DbSet<TEntity> Entities { get; set; }
+    }
+}
