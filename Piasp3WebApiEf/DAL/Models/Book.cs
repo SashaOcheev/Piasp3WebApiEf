f4a17e5683ce2bf86f39c7931dfaa12532f5758a
SashaOcheev <OcheevSasha@gmail.com>
2017-10-23 12:08:26 +0300

    before connecting to db

---
+++ Piasp3WebApiEf/DAL/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using Piasp3WebApiEf.DAL.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Piasp3WebApiEf.DAL.Models
+{
+    public class Book : EntityWithId
+    {
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+        public string Title { get; set; }
+    }
+}
